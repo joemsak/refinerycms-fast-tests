cd75fef3e8a4510903c4b56ff94099c00b967a67
Joe Sak <joe@joesak.com>
2012-03-15 16:58:41 -0500

    Establish the connection and bring in the one line of BaseModel code

--- spec_no_rails/models/planet_spec.rb
+++ spec_no_rails/models/planet_spec.rb
@@ -1,8 +1,12 @@
 require 'active_record'
 
+db = YAML.load(File.open('config/database.yml'))
+ActiveRecord::Base.establish_connection(db['test'])
+
 module Refinery
   module Core
     class BaseModel < ActiveRecord::Base
+      self.abstract_class = true
     end
   end
 end
