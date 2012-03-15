827bfb6fcc9c5678fd8d8be02404d21fad9fc11b
Joe Sak <joe@joesak.com>
2012-03-15 16:50:41 -0500

    Eliminate trailing whitespace

--- vendor/extensions/planets/app/models/refinery/planets/planet.rb
+++ vendor/extensions/planets/app/models/refinery/planets/planet.rb
@@ -1,12 +1,12 @@
 module Refinery
   module Planets
     class Planet < Refinery::Core::BaseModel
-      self.table_name = 'refinery_planets'      
-    
+      self.table_name = 'refinery_planets'
+
       acts_as_indexed :fields => [:name]
 
       validates :name, :presence => true, :uniqueness => true
-              
+
     end
   end
 end
