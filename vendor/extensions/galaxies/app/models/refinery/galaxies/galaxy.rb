0e1861321fc34935be0db4e793e573bbd4ea7984
Joe Sak <joe@joesak.com>
2012-03-15 17:27:57 -0500

    Eliminate trailing whitespace

--- vendor/extensions/galaxies/app/models/refinery/galaxies/galaxy.rb
+++ vendor/extensions/galaxies/app/models/refinery/galaxies/galaxy.rb
@@ -1,12 +1,12 @@
 module Refinery
   module Galaxies
     class Galaxy < Refinery::Core::BaseModel
-      self.table_name = 'refinery_galaxies'      
-    
+      self.table_name = 'refinery_galaxies'
+
       acts_as_indexed :fields => [:name]
 
       validates :name, :presence => true, :uniqueness => true
-              
+
     end
   end
 end
