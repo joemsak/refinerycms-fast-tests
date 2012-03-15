b8c00a838dd64cbfb4f9ec7389fc98620f37458f
Joe Sak <joe@joesak.com>
2012-03-15 16:55:20 -0500

    Run specs, but AR needs connection established

--- spec_no_rails/models/planet_spec.rb
+++ spec_no_rails/models/planet_spec.rb
@@ -12,6 +12,10 @@ require 'planets/app/models/refinery/planets/planet'
 module Refinery
   module Planets
     describe Planet do
+      it "has a name" do
+        planet = Planet.create!(:name => 'Mars')
+        planet.name.should == 'Mars'
+      end
     end
   end
 end
